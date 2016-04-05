5f4c471690ad87233df802af951e62b260638e9b
Ryan Hass <ryan@invalidchecksum.net>
2016-04-05 15:45:00 -0700

    Add buildkite script to build/generate boxes.
    This script was originally written for bento, and updates to more
    intelligently determine the last successful build by querying the
    buildkite API to find the last successful build. It then builds any
    template which it determines has relevant changes.

---
+++ bin/buildkite.rb
@@ -0,0 +1,93 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'net/http'
+require 'uri'
+require 'logger'
+
+BUILDKITE_ACCESS_TOKEN = ENV['BUILDKITE_ACCESS_TOKEN']
+BUILDKITE_PROJECT = ENV['BUILDKITE_PROJECT'] ||= 'bento'
+BUILDKITE_ORGANIZATION = ENV['BUILDKITE_ORGANIZATION_SLUG'] ||= 'chef'
+IGNORED_FILES = %w(
+  gitignore
+  dummy_metadata
+  bin/buildkite.rb
+  bin/compress.py
+  scripts/common
+  .tpl
+  .md
+).freeze
+
+logger = Logger.new(STDOUT)
+
+def buildkite_api_uri(args = {})
+  raise Exception.new('Missing project argument') if args[:project].nil?
+  raise Exception.new('Missing endpoint argument') if args[:endpoint].nil?
+
+  URI::HTTPS.build(
+    host:   'api.buildkite.com',
+    path:   "/v2/organizations/#{BUILDKITE_ORGANIZATION}/pipelines/#{args[:project]}/#{args[:endpoint]}",
+    query:  "access_token=#{BUILDKITE_ACCESS_TOKEN}"
+  )
+end
+
+# Returns an array of builds hashes for the environment defined project.
+def buildkite_builds
+  response = Net::HTTP.get_response(
+      buildkite_api_uri(project: BUILDKITE_PROJECT, endpoint: 'builds')
+  )
+
+  if response.code.to_i >= 400
+    raise Exception.new("Unexpected response from BuildKite API: #{response.code_type}")
+  else
+    JSON.parse(response.body)
+  end
+end
+
+# Returns a string
+def last_passed_build_git_hash
+  git_hash = ''
+
+  buildkite_builds.map do |build|
+    if build['state'] == 'passed'
+      git_hash = build['commit']
+      break
+    end
+  end
+
+  if git_hash.empty?
+    raise Exception.new('Unable to determine the commit hash of the last successful build.')
+  else
+    git_hash
+  end
+end
+
+
+# Return an array of files changed since the last successful build.
+# Sorry for the long function name.
+def changed_files_since_last_passed_build
+  # Cache fileset so we don't need to regenerate the result each time.
+  @changed_files_since_last_passed_build ||= begin
+    # `rev-list commit_x..HEAD` shows the commit hashes for each commit from a
+    # given commit to the current HEAD.
+    # `--objects` shows the actual files which have changed in the series of
+    # commits.
+    # We strip the commit IDs via awk because its cheap and a lazy way to get only
+    # the data back we need.
+    objects = `git rev-list #{last_passed_build_git_hash}..HEAD  --objects | awk '{print $2}'`.split.uniq
+    # Only return objects which are files, or files with a path.
+    objects.select { |object| File.file?(object) && !IGNORED_FILES.include?(object) }
+  end
+end
+
+# Compile the list of platforms whose boxes will be rebuilt.
+buildlist = []
+
+buildlist.concat(changed_files_since_last_passed_build.select { |b| b.include?('.json') })
+buildlist.collect! { |b| b.gsub!('.json', '') }
+
+buildlist.each do |template|
+  logger.info("Building #{template}...")
+  Process.spawn("make vmware/#{template}")
+end
+
